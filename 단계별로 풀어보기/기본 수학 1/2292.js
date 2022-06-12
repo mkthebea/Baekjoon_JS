3ab4d5215e103dc4b07d906860fd8a3eb2b2b5a5
mkthebea <5596molly@naver.com>
2022-06-13 00:52:46 +0900

    기본 수학 1

--- 단계별로 풀어보기/기본 수학 1/2292.js
+++ 단계별로 풀어보기/기본 수학 1/2292.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const input = fs.readFileSync("./dev/stdin").toString().trim();
 const n = parseInt(input);
-
+//(x-2)(x-1) < (n-1)/3 <= x(x-1)을 만족하는 x 찾기
 if (n === 1) {
   console.log(1);
 } else {
@@ -11,9 +11,8 @@ if (n === 1) {
   }
   console.log(i);
 }
-//(x-2)(x-1) < (n-1)/3 <= x(x-1)을 만족하는 x 찾기
 
-//굳이 공식 안써도
+// 굳이 공식 안써도
 // let i = 1;
 // let sum = 1;
 // while (sum < n) {
