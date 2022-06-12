f3f37074c0a961790e969ca52beaf085eb3e7201
mkthebea <5596molly@naver.com>
2022-06-13 00:48:49 +0900

    기존 수학 1

---
+++ 단계별로 풀어보기/기본 수학 1/1712.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString().split(" ");
+const a = parseInt(input[0]); //고정 비용
+const b = parseInt(input[1]); //가변 비용(한 대당)
+const c = parseInt(input[2]); //노트북 가격
+//a + b*x < c*x
+//a/(c-b)<x
+if (c - b <= 0) {
+  console.log(-1);
+} else {
+  console.log(Math.floor(a / (c - b) + 1));
+}
---
+++ 단계별로 풀어보기/기본 수학 1/2292.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString().trim();
+const n = parseInt(input);
+
+if (n === 1) {
+  console.log(1);
+} else {
+  let i = 1;
+  while (n - 1 > 3 * i * (i - 1)) {
+    i++;
+  }
+  console.log(i);
+}
+//(x-2)(x-1) < (n-1)/3 <= x(x-1)을 만족하는 x 찾기
+
+//굳이 공식 안써도
+// let i = 1;
+// let sum = 1;
+// while (sum < n) {
+//   sum += 6 * i;
+//   i++;
+// }
+// 이렇게 단순하게 가능
